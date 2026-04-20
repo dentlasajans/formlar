287174bc77d888a912608afd84fbb1ac81d52ad3
himmetmuhammedk <himmetmuhammedk@gmail.com>
2026-04-20 13:03:12 +0300

    sd

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
@@ -10,10 +9,6 @@ export default defineConfig(({mode}) => {
     plugins: [
       react(), 
       tailwindcss(),
-      nodePolyfills({
-        globals: { Buffer: true, global: true, process: true },
-        protocolImports: true,
-      }),
     ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
